45ad30a148a941633002127ef47a5f55c1f7ae85
NellitG <kamaunelson157@gmail.com>
2025-01-06 17:40:01 +0300

    message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -9,21 +9,21 @@ const routes = [
   { path: "/posts/:id", name: "details", component: DetailsView, props: true },
 ];
 
-/**Initialize here */
+// /**Initialize here */
 const router = createRouter({ history: createWebHistory(), routes });
 
-router.beforeEach(async (to) => {
-  // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ["/login"];
-  const authRequired = !publicPages.includes(to.path);
-  const auth = useAuthStore();
+// router.beforeEach(async (to) => {
+//   // redirect to login page if not logged in and trying to access a restricted page
+//   const publicPages = ["/login"];
+//   const authRequired = !publicPages.includes(to.path);
+//   const auth = useAuthStore();
 
-  /**add function to check if user is logged in */
+//   /**add function to check if user is logged in */
 
-  if (authRequired && !auth.user) {
-    auth.returnUrl = to.fullPath;
-    return "/login";
-  }
-});
+//   if (authRequired && !auth.user) {
+//     auth.returnUrl = to.fullPath;
+//     return "/login";
+//   }
+// });
 
 export default router;
