8a4f0e525d77b2b9e2c152a42e24bfcbe639cf36
NellitG <kamaunelson157@gmail.com>
2025-01-30 13:23:53 +0300

    add a header and cart

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -3,7 +3,7 @@
     <!-- Header section -->
     <header
       :class="{
-        'h-14 lg:h-20 fixed w-full top-0 inset-0 z-20 bg-gradient-to-r from-blue-500 to-yellow-500 transition-transform duration-300': true,
+        'h-14 lg:h-20 fixed w-full top-0 inset-0 z-20 bg-gradient-to-r from-green-900 to-blue-900 transition-transform duration-100': true,
         '-translate-y-full': !isHeaderVisible,
       }"
     >
@@ -39,7 +39,7 @@
           <!-- Navigation items -->
           <ul
             class="flex flex-col lg:flex-row gap-6 lg:gap-8 text-white lg:text-black mt-4 lg:mt-0 lg:items-center px-4 lg:px-0 font-semibold"
-            >
+          >
             <li>
               <router-link
                 to="/"
@@ -69,7 +69,7 @@
                 @click.native="closeMenu"
                 >About Us</router-link
               >
-            </li>            
+            </li>
             <li>
               <router-link
                 to="/contact"
@@ -78,8 +78,46 @@
                 @click.native="closeMenu"
                 >Contact Us</router-link
               >
-            </li>
+            </li>         
           </ul>
+          <!-- Icons container -->
+          <div class="flex items-end justify-end gap-12 ml-20">
+            <!-- Cart icon -->
+            <router-link to="/cart" class="text-white lg:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                />
+              </svg>
+            </router-link>
+
+            <!-- User icon -->
+            <router-link to="/profile" class="text-white lg:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+            </router-link>
+          </div>
         </nav>
       </div>
     </header>
@@ -116,7 +154,8 @@ export default {
     },
     handleScroll() {
       const currentScrollY = window.scrollY;
-      this.isHeaderVisible = currentScrollY < this.lastScrollY || currentScrollY <= 0;
+      this.isHeaderVisible =
+        currentScrollY < this.lastScrollY || currentScrollY <= 0;
       this.lastScrollY = currentScrollY;
     },
   },
@@ -142,10 +181,12 @@ nav {
   transition: all 0.3s ease-in-out;
 }
 
-/* Adjust visibility for donation button */
+/* Adjust visibility for icons on small screens */
 @media (max-width: 1024px) {
-  .donate-button {
-    display: none;
+  .icons-container {
+    display: flex;
+    gap: 1rem;
+    margin-left: auto;
   }
 }
 </style>
