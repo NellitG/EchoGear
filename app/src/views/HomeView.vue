96f988592eeb9018efb6ec8160a8268bf3a14900
NellitG <kamaunelson157@gmail.com>
2025-02-27 21:24:50 +0300

    removed comments

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,19 +1,11 @@
 <template>
   <Hero />
-  <!-- <Empower /> -->
-  <!-- <calltoaction /> -->
-  <!-- <impact /> -->
-  <!-- <Programs /> -->
   <Card />
 </template>
 
 <script setup>
 import Hero from "@/components/Hero.vue";
-// import Programs from "@/components/Programs.vue";
-// import impact from "@/components/impact.vue";
-// import calltoaction from "@/components/calltoaction.vue";
-// import Cards from "@/components/Cards.vue";
-// import Empower from "../components/Empower.vue";
+
 import Card from "@/components/Card.vue";
 </script>
 
