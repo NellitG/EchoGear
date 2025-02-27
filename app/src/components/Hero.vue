22fa4348ad15718589b5d1fa172d495eae242600
NellitG <kamaunelson157@gmail.com>
2025-02-27 21:15:08 +0300

    put and color hover button

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -36,7 +36,7 @@
           </a>
           <a
             href="#"
-            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+            class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 rounded-lg hover:bg-orange-600 focus:ring-4 focus:ring-gray-100"
           >
             Speak to Sales
           </a>
