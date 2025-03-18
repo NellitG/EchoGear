11933d1b7f6d79fdebfba911a1c536610d99bb1f
NellitG <kamaunelson157@gmail.com>
2025-03-18 19:39:35 +0300

    client component called in homeview

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <Hero />
   <Card />
+  <Client />
 </template>
 
 <script setup>
 import Hero from "@/components/Hero.vue";
 
 import Card from "@/components/Card.vue";
+import Client from "@/components/Client.vue";
 </script>
 
 <style lang="scss" scoped></style>
