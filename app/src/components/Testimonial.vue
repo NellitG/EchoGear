23bacddefb4646426a8b50a5299b9f7cfb027c7e
NellitG <kamaunelson157@gmail.com>
2025-03-19 19:34:26 +0300

    added testimonials

---
+++ app/src/components/Testimonial.vue
@@ -0,0 +1,24 @@
+<template>
+    <div className="flex flex-col items-center justify-center p-8 bg-white border border-gray-300 rounded-lg shadow-lg">
+    
+      <div className="flex space-x-1 text-black">
+            <span key={index} className="text-xl">★</span>
+      </div>
+
+   
+      <p className="mt-4 text-center text-lg font-semibold text-black">
+        "Using this API has transformed how we engage with music. It’s intuitive and has opened up endless possibilities for our platform!"
+      </p>
+      <div className="flex items-center mt-4 space-x-3">
+        <div className="w-10 h-10 bg-gray-300 rounded-full flex items-center justify-center">
+          <span className="text-gray-500 text-sm font-bold">🚀</span>
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-gray-700">Alex Johnson</p>
+          <p className="text-xs text-gray-500">CEO, MusicHub</p>
+        </div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Webflow_logo.svg" alt="Webflow" className="h-5" />
+      </div>
+    </div>
+</template>
+  
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <Hero />
   <Card />
+  <Testimonial />
   <Client />
 </template>
 
@@ -9,6 +10,7 @@ import Hero from "@/components/Hero.vue";
 
 import Card from "@/components/Card.vue";
 import Client from "@/components/Client.vue";
+import Testimonial from "../components/Testimonial.vue";
 </script>
 
 <style lang="scss" scoped></style>
