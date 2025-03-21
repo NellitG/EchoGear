efd3e539e95b60fa1074ccb8366d2638daf0b1e1
NellitG <kamaunelson157@gmail.com>
2025-03-21 16:05:02 +0300

    added calltoaction

---
+++ app/src/components/Calltoaction.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="border-gray-300 p-6 md:p-10 rounded-lg mt-10">
+      <div class="max-w-6xl mx-auto flex flex-col md:flex-row items-center">
+        
+        <!-- Left Side: Text Content -->
+        <div class="md:w-1/2 space-y-4">
+          <h2 class="text-5xl md:text-5xl font-gloria text-gray-900">
+            Start Your Music<br> <span class="text-orange-600">Journey Today</span>
+          </h2>
+          <p class="text-gray-700">
+            Sign up for a free trial and explore the endless possibilities of our<br>music API.
+          </p>
+          <!-- Buttons -->
+          <div class="flex gap-4">
+            <a href="#" class="bg-green-400 text-white px-6 py-3 font-semibold rounded-lg shadow-md transition">
+              Sign Up
+            </a>
+            <a href="#" class="px-6 py-3 font-semibold rounded-lg text-gray-900 hover:bg-orange-500 transition">
+              Learn More
+            </a>
+          </div>
+        </div>
+  
+        <!-- Right Side: Image -->
+        <div class="md:w-1/2 mt-6 md:mt-0">
+          <img src="https://media.istockphoto.com/id/2158828258/photo/business-partners-in-meeting.jpg?s=612x612&w=0&k=20&c=2q6wFvu9dDxce3elMWdlk2eNJFuLqKedarQBHe5TORM=" alt="Music Learning" class="rounded-lg shadow-lg w-full object-cover" />
+        </div>
+  
+      </div>
+    </section>
+  </template>
+  
