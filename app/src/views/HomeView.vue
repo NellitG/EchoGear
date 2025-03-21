950f41433b4b589925a66fb0c0a65a2704790c7f
NellitG <kamaunelson157@gmail.com>
2025-03-21 16:08:43 +0300

    added all components in homeview

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,16 +1,21 @@
 <template>
   <Hero />
-  <Card />
+  <Streaming />
+  <!-- <Card /> -->
   <Testimonial />
+  <Section />
   <Client />
+  <Calltoaction />
 </template>
 
 <script setup>
+import Section from "@/components/Section.vue";
 import Hero from "@/components/Hero.vue";
-
 import Card from "@/components/Card.vue";
 import Client from "@/components/Client.vue";
 import Testimonial from "../components/Testimonial.vue";
+import Streaming from "../components/Streaming.vue";
+import Calltoaction from "../components/Calltoaction.vue";
 </script>
 
 <style lang="scss" scoped></style>
