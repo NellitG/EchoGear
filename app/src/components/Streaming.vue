1dc982f1ab8610f3065fb63280267b11ea3dc57a
NellitG <kamaunelson157@gmail.com>
2025-03-25 14:13:05 +0300

    updated streaming.vue

--- app/src/components/Streaming.vue
+++ app/src/components/Streaming.vue
@@ -1,57 +1,91 @@
 <template>
-    <section className="py-12 text-center">
-  
-      <h2 className="text-2xl md:text-4xl font-gloria text-gray-900">
-       <span class="text-orange-400"> Unlock the Power of Music with Our</span> <br /> Streaming API Services
-      </h2>
+  <section className="py-12 text-center">
+    <h2 className="text-2xl md:text-4xl font-gloria text-gray-900">
+      <span class="text-orange-400"> Unlock the Power of Music with Our</span>
+      <br />
+      Streaming API Services
+    </h2>
 
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
-       
-        <div className="p-6 border border-gray-300 rounded-lg shadow-md">
-          <div className="w-full h-40 bg-gray-200 flex items-center justify-center">
-            <img src="" alt="Music Streaming" className="w-20 h-20 object-contain" />
-          </div>
-          <h3 className="mt-4 text-lg font-semibold text-gray-900">
-            Stream, Create, and Share Your Favorite Music Effortlessly
-          </h3>
-          <p className="mt-2 text-sm text-gray-600">
-            <span class="text-orange-600">Our Music Streaming API provides seamless access</span> to a vast library of songs.
-          </p>
-          <a href="#" className="mt-3 inline-block text-sm font-semibold text-gray-800">
-           <span class="text-green-500">Explore</span>  &rarr;
-          </a>
+    <div
+      className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto"
+    >
+      <div className="p-6 border-2 hover:border-green-500 rounded-lg shadow-md">
+        <div
+          className="w-full h-40 flex items-center justify-center overflow-hidden"
+        >
+          <img
+            src="https://img.freepik.com/free-photo/dreamy-african-girl-listening-music-big-headphones-laughing-winsome-black-female-model-posing-with-longboard-pastel_197531-11151.jpg?uid=R141465363&ga=GA1.1.487577296.1727511220&semt=ais_hybrid"
+            alt="Music Streaming"
+            className="w-full h-full object-cover rounded-md"
+          />
         </div>
-
-    
-        <div className="p-6 border border-gray-300 rounded-lg shadow-md">
-          <div className="w-full h-40 bg-gray-200 flex items-center justify-center">
-            <img src="" alt="Music Streaming" className="w-20 h-20 object-contain" />
-          </div>
-          <h3 className="mt-4 text-lg font-semibold text-gray-900">
-            Build Custom Playlists and Discover New Artists with Ease
-          </h3>
-          <p className="mt-2 text-sm text-gray-600">
-            <span class="text-orange-600">Create personalized playlists and follow your</span> favorite artists to enhance your music experience.
-          </p>
-          <a href="#" className="mt-3 inline-block text-sm font-semibold text-gray-800">
-            <span class="text-green-500">Create</span>&rarr;
-          </a>
-        </div>
-
-        <div className="p-6 border border-gray-300 rounded-lg shadow-md">
-          <div className="w-full h-40 bg-gray-200 flex items-center justify-center">
-            <img src="" alt="Music Streaming" className="w-20 h-20 object-contain" />
-          </div>
-          <h3 className="mt-4 text-lg font-semibold text-gray-900">
-            Engage with a Community of Music Lovers and Share Your Passion
-          </h3>
-          <p className="mt-2 text-sm text-gray-600">
-            <span class="text-orange-600">Join a vibrant community where you can share </span>music and connect with others.
-          </p>
-          <a href="#" className="mt-3 inline-block text-sm font-semibold text-gray-800">
-           <span class="text-green-500">Join</span>&rarr;
-          </a>
-        </div>
+        <h3 className="mt-4 text-lg font-semibold text-gray-900">
+          Stream, Create, and Share Your Favorite Music Effortlessly
+        </h3>
+        <p className="mt-2 text-sm text-gray-600">
+          <span className="text-orange-600">
+            Our Music Streaming API provides seamless access to a vast library of songs. </span
+          >
+        </p>
+        <a
+          href="#"
+          className="mt-3 inline-block text-sm font-semibold text-gray-800"
+        >
+          <span className="text-green-500">Explore</span> &rarr;
+        </a>
       </div>
-    </section>
+
+      <div className="p-6 border-2 hover:border-green-500 rounded-lg shadow-md">
+        <div
+          className="w-full h-40 flex items-center justify-center overflow-hidden"
+        >
+          <img
+            src="https://img.freepik.com/free-photo/dreamy-african-girl-listening-music-big-headphones-laughing-winsome-black-female-model-posing-with-longboard-pastel_197531-11151.jpg?uid=R141465363&ga=GA1.1.487577296.1727511220&semt=ais_hybrid"
+            alt="Music Streaming"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+        <h3 className="mt-4 text-lg font-semibold text-gray-900">
+         Build CustomPlaylist and Discover New Artists with Ease
+        </h3>
+        <p className="mt-2 text-sm text-gray-600">
+          <span className="text-orange-600">
+            Create personalized playlists and follow your favorite artists to enhance your music experience. </span
+          >
+        </p>
+        <a
+          href="#"
+          className="mt-3 inline-block text-sm font-semibold text-gray-800"
+        >
+          <span className="text-green-500">Create</span> &rarr;
+        </a>
+      </div>
+
+      <div className="p-6 border-2 hover:border-green-500 rounded-lg shadow-md">
+        <div
+          className="w-full h-40 flex items-center justify-center overflow-hidden"
+        >
+          <img
+            src="https://img.freepik.com/free-photo/dreamy-african-girl-listening-music-big-headphones-laughing-winsome-black-female-model-posing-with-longboard-pastel_197531-11151.jpg?uid=R141465363&ga=GA1.1.487577296.1727511220&semt=ais_hybrid"
+            alt="Music Streaming"
+            className="w-full h-full object-cover rounded-md"
+          />
+        </div>
+        <h3 className="mt-4 text-lg font-semibold text-gray-900">
+          Engage with a Community of Music Lovers and Share Your Passion
+        </h3>
+        <p className="mt-2 text-sm text-gray-600">
+          <span className="text-orange-600">
+           Join a vibrant community where you can share your favorite songs, playlists, and artists with other music enthusiasts.</span>
+        </p>
+        <a
+          href="#"
+          className="mt-3 inline-block text-sm font-semibold text-gray-800"
+        >
+          <span className="text-green-500">Join</span> &rarr;
+        </a>
+      </div>
+
+    </div>
+  </section>
 </template>
